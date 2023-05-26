130bb230e953b5fceeb190d87ee7e0dce9300bd9
[Github <[Github>
2023-05-26 15:45:56 +0900

    데이터 1차가공, 필요한 항목/항목순서정렬/distance 'm'화/distance 가까운순으로 정렬

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,9 @@ export class PlacesController {
 
   @Get('/:query')
   async searchPlaces(@Param('query') query: string): Promise<any> {
-    const result = await this.kakaoMapService.searchPlaces(query);
+    const userLocation = { latitude: 37.535744, longitude: 127.074304 }; // 사용자 위치 정보 입력
+
+    const result = await this.kakaoMapService.searchPlaces(query, userLocation);
     // 여기서 결과를 처리하거나 필요한 로직을 추가하세요.
     return result;
   }
